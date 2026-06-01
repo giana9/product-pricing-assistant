98031ecc8792d2f037df486dbd5852f3def7209f
Giana Cresta <crestagiana@gmail.com>
2026-05-31 21:38:33 -0400

    added calculations

--- product-pricing-assistant.js
+++ product-pricing-assistant.js
@@ -5,3 +5,10 @@ let basePrice = 20;
 let discountRate = .1;
 let salesTaxRate = .07;
 let fixedMonthlyCosts = 1500;
+
+// Calculations
+let discountedPrice = basePrice * (1 - discountRate);
+let finalPriceWithTax = discountedPrice * (1 + salesTaxRate);
+let profitPerUnit = finalPriceWithTax - costPerUnit;
+let breakEvenUnits = Math.ceil(fixedMonthlyCosts / profitPerUnit);
+let isProfitablePerUnit = profitPerUnit > 0;
